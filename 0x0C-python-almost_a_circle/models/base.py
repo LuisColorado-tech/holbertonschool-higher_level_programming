06fdfe8aeb47a05185dce7e26fe58832b1147982
LuisColorado-tech <luiscolorado@outlook.com>
2021-06-08 13:50:22 -0500

    Add files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
+"""Define Base class
 """
-Module base.py
-"""
+
 
 import json
+import csv
+
 
 class Base:
-    """Base class"""
+    """Define Base class
+    """
     __nb_objects = 0
+
     def __init__(self, id=None):
-        if id is not None:
+        """Initialisation
+        Args:
+            id (int, None): integer identification. Defaults to None.
+        """
+        if id:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+            
