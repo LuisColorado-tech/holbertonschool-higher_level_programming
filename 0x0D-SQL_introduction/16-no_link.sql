a5b752dd191e0cc5645017b461e1d87330326099
LuisColorado-tech <luiscolorado@outlook.com>
2021-07-13 21:13:20 -0500

    add file

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
--- display both the score and the name (in this order)
 SELECT score, name
 FROM second_table
+WHERE name IS NOT NULL
 ORDER BY score DESC
